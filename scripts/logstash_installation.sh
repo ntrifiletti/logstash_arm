a56d8e9c257192f3d5adb4bbe94ce4e8e31eecef
aravindan <aravindan.acct@gmail.com>
2022-08-24 11:53:15 +0530

    elk version changed

--- scripts/logstash_installation.sh
+++ scripts/logstash_installation.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
 sudo apt-get update
 sudo apt-get install -y default-jdk
 sudo apt-get install -y elasticsearch
