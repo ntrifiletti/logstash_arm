edd457f32569a2aac7c7f193134bc1972102bfeb
aravindan <aravindan.acct@gmail.com>
2022-08-24 16:26:36 +0530

    script and template updated to get userdata

--- scripts/logstash_installation.sh
+++ scripts/logstash_installation.sh
@@ -16,8 +16,8 @@ touch /tmp/test.txt
 apt install -y libxml2-utils
 apt install -y jq
 xmllint --xpath "//*[local-name()='CustomData']" /var/lib/waagent/ovf-env.xml | sed -e 's/ns1:/ /g' | sed -e 's/ //g' > file.xml
-$omsid = xmllint --xpath 'string(//CustomData)' file.xml | base64 --decode | jq '.OMSWorkspaceID'
-$omskey = xmllint --xpath 'string(//CustomData)' file.xml | base64 --decode | jq '.OMSWorkspaceKey'
+omsid = xmllint --xpath 'string(//CustomData)' file.xml | base64 --decode | jq '.OMSWorkspaceID'
+omskey = xmllint --xpath 'string(//CustomData)' file.xml | base64 --decode | jq '.OMSWorkspaceKey'
 sudo sed -i "s/workspace_id => \"junk\"/workspace_id => $omsid /g" /etc/logstash/conf.d/waf.conf
 sudo sed -i "s/workspace_id => \"test\"/workspace_id => $omskey /g" /etc/logstash/conf.d/waf.conf
 <<logstash
