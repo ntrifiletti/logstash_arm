e6e92ef93c85c6584c84ceeb271352c719fef0a1
aravindan <aravindan.acct@gmail.com>
2022-08-24 18:35:02 +0530

    edits to the install script

--- scripts/logstash_installation.sh
+++ scripts/logstash_installation.sh
@@ -15,16 +15,27 @@ touch /tmp/test.txt
 apt install -y libxml2-utils && apt install -y jq
 xmllint --xpath "//*[local-name()='CustomData']" /var/lib/waagent/ovf-env.xml | sed -e 's/ns1:/ /g' | sed -e 's/ //g' > /tmp/file.xml
 cat /tmp/file.xml
-omsid = $(xmllint --xpath 'string(//CustomData)' /tmp/file.xml | base64 --decode | jq '.OMSWorkspaceID')
-omskey = $(xmllint --xpath 'string(//CustomData)' /tmp/file.xml | base64 --decode | jq '.OMSWorkspaceKey')
+omsid=$(xmllint --xpath 'string(//CustomData)' /tmp/file.xml | base64 --decode | jq '.OMSWorkspaceID')
+omskey=$(xmllint --xpath 'string(//CustomData)' /tmp/file.xml | base64 --decode | jq '.OMSWorkspaceKey')
+
+echo "replacing the waf.conf file contents"
 
 echo "OMS ID: \n"
 echo $omsid
-echo "\nOMS ID: \n"
+if sudo sed -i "s/workspace_id => \"junk\"/workspace_id => $omsid /g" /etc/logstash/conf.d/waf.conf ; then
+    echo "Replaced OMS ID"
+else
+    echo "Error.. please check command"
+fi
+
+echo "\nOMS Key: \n"
 echo $omskey
-echo "replacing the waf.conf file contents"
-sudo sed -i "s/workspace_id => \"junk\"/workspace_id => $omsid /g" /etc/logstash/conf.d/waf.conf
-sudo sed -i "s/workspace_id => \"test\"/workspace_id => $omskey /g" /etc/logstash/conf.d/waf.conf
+
+if sudo sed -i "s/workspace_key => \"test\"/workspace_id => $omskey /g" /etc/logstash/conf.d/waf.conf ; then
+    echo "Replaced OMS Key"
+else
+    echo "Error replacinig the key.. please check command"
+fi
 <<logstash
 sudo systemctl start logstash
 sudo systemctl enable logstash
