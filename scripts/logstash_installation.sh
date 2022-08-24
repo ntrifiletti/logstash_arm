b1d97f831f52df2558061f8be8ccbd3d2fe6d244
aravindan <aravindan.acct@gmail.com>
2022-08-24 15:51:53 +0530

    script and template updated to get userdata

--- scripts/logstash_installation.sh
+++ scripts/logstash_installation.sh
@@ -12,10 +12,15 @@ sudo apt-get install -y logstash
 wget https://raw.githubusercontent.com/aravindan-acct/logstash_arm/main/scripts/waf.conf
 sudo mv waf.conf /etc/logstash/conf.d/
 sudo /usr/share/logstash/bin/logstash-plugin install microsoft-logstash-output-azure-loganalytics
+touch /tmp/test.txt
+apt install -y libxml2-utils
+apt install -y jq
+xmllint --xpath "//*[local-name()='CustomData']" /var/lib/waagent/ovf-env.xml | sed -e 's/ns1:/ /g' | sed -e 's/ //g' > file.xml
+$omsid = xmllint --xpath 'string(//CustomData)' file.xml | base64 --decode | jq '.OMSWorkspaceID'
+$omskey = xmllint --xpath 'string(//CustomData)' file.xml | base64 --decode | jq '.OMSWorkspaceKey'
+sudo sed -i "s/workspace_id => \"junk\"/workspace_id => $omsid /g" /etc/logstash/conf.d/waf.conf
+sudo sed -i "s/workspace_id => \"test\"/workspace_id => $omskey /g" /etc/logstash/conf.d/waf.conf
 <<logstash
 sudo systemctl start logstash
 sudo systemctl enable logstash
 logstash
-touch /tmp/test.txt
-
-
